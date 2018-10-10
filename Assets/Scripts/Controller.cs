ad1b77e1b67749cfdb963759457026e3ffce163e
Benjamin Armour <barmour@mail.pima.edu>
2018-10-10 13:33:49 -0700

    Ray cast for Character and slipperiness.
    Character now uses raycast to jump.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,38 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour {
 
-    public Transform transformToFlip;
+    public Transform Player;
     public BoxCollider2D Mycollidor;
     public Rigidbody2D Ball;
     public float speed;
     public float jumpPower = 1f;
     public Animator animator;
-    public Vector2 scale;
-    public Vector2 originscale;
-    public Vector2 direction;
-    public Vector2 newoffset;
-    public Vector2 oldoffset;
-    public CollisionDetectionMode2D TheGround;
-    int contacts = 0;
-    public AudioSource Beat;
+    private Vector2 direction;
+    private Vector3 offset;
+   // private ray GroundIsHere;
+    
+    private CollisionDetectionMode2D TheGround;
+    float contacts = 0f;
+    
 
 
     private float buttonX;
     private bool buttonY;
-    private bool buttonE;
-    private bool buttonR;
     // Use this for initialization
     void Start()
     {
         PlayerPrefs.SetInt("lastLevel", SceneManager.GetActiveScene().buildIndex);
 
-        scale = new Vector2(.5f, .5f);
-        originscale = new Vector2(.6f, 1.3f);
         direction = new Vector2(0f, 1f);
         direction = direction.normalized;
-        newoffset = new Vector2(-.03f, -.7f);
-        oldoffset = new Vector2(-.03f, -.3f);
         TheGround = CollisionDetectionMode2D.Continuous;
+        offset = new Vector3(0f,.5f,0f);
+
         
     }
 
@@ -44,50 +39,52 @@ public class Controller : MonoBehaviour {
     void Update()
     {
 
-        RaycastHit2D hit = Physics2D.Raycast(this.gameObject.transform.position, direction);
-        
+       
+    	//GroundIsHere = new Ray(this.transform.position, vector3.down);
+    	RaycastHit2D hit = Physics2D.Raycast(Player.position- offset, Vector2.down);
+
+    //	if( hit.distance < .1f) {
+   // 		contacts = 1;
+  //  	}
+   // 	else{
+   // 		contacts = 0;
+   // 	}
+
+    	Debug.DrawRay(Player.position - offset, Vector2.down);
+    	if (hit.collider != null)
+            {
+            	if (hit.distance < .1f){
+                Debug.Log(hit.collider.name);
+                contacts = 1;
+            }
+        }
+                else{
+                	contacts = 0;
+                }
+            
+
+
         buttonX = Input.GetAxis("Horizontal");
         buttonY = Input.GetButtonDown("Jump");
-        buttonE = Input.GetButtonDown("Fire1");
-        buttonR = Input.GetButtonDown("Fire2");
+        
         if (buttonY){
             if (Input.GetButtonDown("Jump"))
             {
                 if (contacts > 0) Jump();
+                contacts = 0;
             }
         }
-        if (buttonE)
-        {           
-            Mycollidor.size = scale;
-            animator.SetBool("Crouch", true);
-            Mycollidor.offset = newoffset;
-            speed = 5f;
-            Beat.Play();
-        }
-        if (buttonR)
-        {
-            if (hit.distance <= .5)
-            {
-
-            }
-            else
-            {
-                Mycollidor.size = originscale;
-                animator.SetBool("Crouch", false);
-                Mycollidor.offset = oldoffset;
-                speed = 10f;
-                Beat.Stop();
-            }
-        }
+        
+        
         animator.SetFloat("Hmove", buttonX);
 
         if (buttonX < 0f)
         {
-            transformToFlip.eulerAngles = new Vector3(0f, 180f, 0f);
+            Player.eulerAngles = new Vector3(0f, 180f, 0f);
         }
         if (buttonX > 0f)
         {
-            transformToFlip.eulerAngles = new Vector3(0f, 0f, 0f);
+            Player.eulerAngles = new Vector3(0f, 0f, 0f);
         }
     }
 
@@ -101,24 +98,24 @@ public class Controller : MonoBehaviour {
         Ball.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
     }
 
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "Floor")
-        {
+   // void OnCollisionEnter2D(Collision2D col)
+   // {
+    //    if (col.gameObject.tag == "Floor")
+     //   {
             
-            contacts++;
-        }
-        else
-        {
+     //       contacts = 1;
+     //   }
+    //    else
+    //    {
             
-        }
-    }
-    void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "Floor")
-        {
-            contacts = 0;
-        }
-    }
+   //     }
+  //  }
+  //  void OnCollisionExit2D(Collision2D col)
+  //  {
+   //     if (col.gameObject.tag == "Floor")
+    //    {
+         //   contacts = 0;
+    //    }
+  //  }
 
 }
