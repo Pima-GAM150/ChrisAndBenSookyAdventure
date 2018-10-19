7fbacc557a0e46b6ae3461ec09f9458626147b2d
Benjamin Armour <barmour@mail.pima.edu>
2018-10-18 17:14:19 -0700

    Random level Select added

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour {
+	public GameObject[] Levels;
+	private float Randomchoice;
+	private int Intchoice;
+	
+	void Start () {
+		Randomchoice = Random.Range(1f,10f);
+		if( Randomchoice < 5){
+			Intchoice = 0;
+		}
+		else {
+			Intchoice = 1;
+		}
+		Levels[Intchoice].SetActive(true);
+		print(Randomchoice);
+	}
+	
+	
+}
